e0669f7e23ecd5c131b9195371a8e4798a2b743f
zhangweijin <909312382@qq.com>
2019-05-31 13:29:40 +0800

    数据库修改

--- application/database.php
+++ application/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'wechatShop',
+    'database'        => 'tp5shop',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'LuYao@03127932678',
+    'password'        => 'root',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
